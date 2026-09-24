9ffeb9bd3cd8fde93ea3e7e9cc197f25f8a4042a
pinalworkforce1-del <pinal.workforce1@gmail.com>
2026-09-24 10:23:15 -0700

    Bump Level Up offline client to v1.0.5

--- public/offline-client.js
+++ public/offline-client.js
@@ -1,6 +1,6 @@
 (() => {
   if (!("serviceWorker" in navigator)) return;
-  const CLIENT_ENGINE_VERSION="1.0.4";
+  const CLIENT_ENGINE_VERSION="1.0.5";
   const script=document.currentScript;
   const localRoot=script?new URL("./",script.src).pathname:new URL("./",location.href).pathname;
 
